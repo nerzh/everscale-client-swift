29632e4d540b7869c643c711f680d5ed7317fcfe
Oleh Hudeichuk <emptystamp@gmail.com>
2020-11-19 01:32:56 +0200

    add tests for client module

--- Sources/TonClientSwift/Client/TonClient.swift
+++ Sources/TonClientSwift/Client/TonClient.swift
@@ -6,6 +6,7 @@ import Foundation
 
 public final class TSDKClientModule {
 
+    public let module: String = "client"
     private let binding: TSDKBindingModule
     public let config: TSDKClientConfig
     public var abi: TSDKAbiModule
@@ -30,5 +31,32 @@ public final class TSDKClientModule {
 
     public func destroy() {
         binding.destroyContext()
+    }
+
+    public func get_api_reference(_ handler: @escaping (TSDKBindingResponse<TSDKResultOfGetApiReference, TSDKClientError, TSDKDefault>) -> Void) {
+        let method: String = "get_api_reference"
+        binding.requestLibraryAsync(methodName(module, method)) { (requestId, params, responseType, finished) in
+            var response: TSDKBindingResponse<TSDKResultOfGetApiReference, TSDKClientError, TSDKDefault> = .init()
+            response.update(requestId, params, responseType, finished)
+            BindingStore.responseQueue.async { handler(response) }
+        }
+    }
+
+    public func version(_ handler: @escaping (TSDKBindingResponse<TSDKResultOfVersion, TSDKClientError, TSDKDefault>) -> Void) {
+        let method: String = "version"
+        binding.requestLibraryAsync(methodName(module, method)) { (requestId, params, responseType, finished) in
+            var response: TSDKBindingResponse<TSDKResultOfVersion, TSDKClientError, TSDKDefault> = .init()
+            response.update(requestId, params, responseType, finished)
+            BindingStore.responseQueue.async { handler(response) }
+        }
+    }
+
+    public func build_info(_ handler: @escaping (TSDKBindingResponse<TSDKResultOfBuildInfo, TSDKClientError, TSDKDefault>) -> Void) {
+        let method: String = "build_info"
+        binding.requestLibraryAsync(methodName(module, method)) { (requestId, params, responseType, finished) in
+            var response: TSDKBindingResponse<TSDKResultOfBuildInfo, TSDKClientError, TSDKDefault> = .init()
+            response.update(requestId, params, responseType, finished)
+            BindingStore.responseQueue.async { handler(response) }
+        }
     }
 }
