1bc4d2f5e801bd013ac68d57a07a3d95b84195ad
Oleh Hudeichuk <emptystamp@gmail.com>
2021-08-15 15:06:27 +0300

    fix comment

--- Sources/TonClientSwift/Utils/Utils.swift
+++ Sources/TonClientSwift/Utils/Utils.swift
@@ -19,7 +19,7 @@ public final class TSDKUtilsModule {
     }
 
     /// Validates and returns the type of any TON address.
-    /// Address types are the following`0:919db8e740d50bf349df2eea03fa30c385d846b991ff5542e67098ee833fc7f7` - standart TON address mostcommonly used in all cases. Also called as hex addres`919db8e740d50bf349df2eea03fa30c385d846b991ff5542e67098ee833fc7f7` - account ID. A part of fulladdress. Identifies account inside particular workchain`EQCRnbjnQNUL80nfLuoD+jDDhdhGuZH/VULmcJjugz/H9wam` - base64 address. Also called "user-friendly".
+    /// Address types are the following`0:919db8e740d50bf349df2eea03fa30c385d846b991ff5542e67098ee833fc7f7` - standard TON address mostcommonly used in all cases. Also called as hex address`919db8e740d50bf349df2eea03fa30c385d846b991ff5542e67098ee833fc7f7` - account ID. A part of fulladdress. Identifies account inside particular workchain`EQCRnbjnQNUL80nfLuoD+jDDhdhGuZH/VULmcJjugz/H9wam` - base64 address. Also called "user-friendly".
     /// Was used at the beginning of TON. Now it is supported for compatibility
     public func get_address_type(_ payload: TSDKParamsOfGetAddressType, _ handler: @escaping (TSDKBindingResponse<TSDKResultOfGetAddressType, TSDKClientError, TSDKDefault>) -> Void
     ) {
